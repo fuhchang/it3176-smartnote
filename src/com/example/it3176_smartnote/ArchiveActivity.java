d97211b904a212718d3539478158a09913ae13dd
Sherry <sherrylau94@gmail.com>
2014-06-18 16:34:00 +0800

    Commit. Completed Archive.

--- src/com/example/it3176_smartnote/ArchiveActivity.java
+++ src/com/example/it3176_smartnote/ArchiveActivity.java
@@ -1,5 +1,58 @@
 package com.example.it3176_smartnote;
 
-public class ArchiveActivity {
+import java.util.ArrayList;
+
+import com.SQLiteController.it3176.SQLiteController;
+import com.example.it3176_smartnote.model.Note;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+
+public class ArchiveActivity extends Activity {
+	ListView list;
 	
+	ArrayList<Note> resultArray = new ArrayList<Note>();
+	ArrayList<Note> tempArray = new ArrayList<Note>();
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+        SQLiteController controller = new SQLiteController(this);
+        controller.open();
+        
+        ArrayList<Note> temptArray = controller.retrieveNotes();
+        controller.close();
+        
+        for(int i = 0; i < temptArray.size(); i++){
+        	if(temptArray.get(i).getNote_status().equals("archive")){
+        		resultArray.add(temptArray.get(i));
+        	}
+        }
+        
+        noteList notelist = new noteList(ArchiveActivity.this, resultArray);
+        list = (ListView) findViewById(R.id.noteListView);
+        list.setAdapter(notelist);
+        
+        list.setOnItemClickListener(new OnItemClickListener(){
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+				Intent intent = new Intent(getApplicationContext(), ArchiveDetail.class);
+				ArrayList<String> selectedNote = new ArrayList<String>();
+				selectedNote.add(resultArray.get(position).getNote_name());
+				selectedNote.add(resultArray.get(position).getNote_content());
+				selectedNote.add(resultArray.get(position).getNote_category());
+				selectedNote.add(resultArray.get(position).getNote_date());
+				intent.putStringArrayListExtra("resultArray", selectedNote);
+				intent.putExtra("note_id", Integer.toString(resultArray.get(position).getNote_id()));
+		        startActivity(intent);
+			}
+        });
+	}
 }
---
+++ src/com/example/it3176_smartnote/ArchiveDetail.java
@@ -0,0 +1,85 @@
+package com.example.it3176_smartnote;
+
+import java.util.ArrayList;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.SQLException;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.SQLiteController.it3176.SQLiteController;
+import com.example.it3176_smartnote.model.Note;
+
+public class ArchiveDetail extends Activity {
+	CheckBox cb_remember_setting;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.note_detail);
+		ArrayList<String> list = getIntent().getStringArrayListExtra("resultArray");
+		EditText etTitle = (EditText) findViewById(R.id.noteTitleET);
+		etTitle.setText(list.get(0).toString());
+		EditText etContent = (EditText) findViewById(R.id.contentTxt);
+		etContent.setText(list.get(1).toString());
+		TextView tvCate = (TextView) findViewById(R.id.cateChoice);
+		tvCate.setText(list.get(2).toString());
+		TextView timeTV = (TextView) findViewById(R.id.txtTime);
+		timeTV.setText(list.get(3).toString());
+		
+		ActionBar actionBar	= getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.archive_detail, menu);
+		
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		
+		if(item.getItemId() == R.id.btn_reactivate) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(ArchiveDetail.this);
+			builder.setTitle("Restore").setMessage("This note will be restored.");
+			
+			builder.setPositiveButton("Ok", new DialogInterface.OnClickListener(){
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					Note note = new Note(Integer.parseInt(getIntent().getStringExtra("note_id")));
+					SQLiteController controller = new SQLiteController(ArchiveDetail.this);
+					try{
+						controller.open();
+						controller.reactivateNote(note);
+					} catch(SQLException e){
+						System.out.println(e);
+					} finally{
+						controller.close();
+						Intent refresh = new Intent(ArchiveDetail.this, ArchiveActivity.class);
+						startActivity(refresh);
+					}
+				}
+			});
+			builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+				@Override
+				public void onClick(DialogInterface dialog, int which) {}
+			});
+			AlertDialog dialog = builder.create();
+			dialog.show();
+		}
+		
+		return super.onOptionsItemSelected(item);
+	}
+}
